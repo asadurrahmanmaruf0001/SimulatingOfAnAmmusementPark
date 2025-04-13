8a8dde107d0f6a841d7d8b7b4245db57be528234
akib007-prog <akibhassan267@gmail.com>
2025-04-13 08:05:08 +0600

    maintenance fxml done

---
+++ src/main/java/com/example/demo2/Akib/ResponseTeamFxml.java
@@ -0,0 +1,21 @@
+package com.example.demo2.Akib;
+
+public class ResponseTeamFxml
+{
+    @javafx.fxml.FXML
+    private Label messageLabel;
+    @javafx.fxml.FXML
+    private TextField areaInvolvedTextField;
+    @javafx.fxml.FXML
+    private TextArea descriptionFieldTextArea;
+    @javafx.fxml.FXML
+    private ComboBox priorityLevelComboBox;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void assignButtonOnAction(ActionEvent actionEvent) {
+    }
+}
