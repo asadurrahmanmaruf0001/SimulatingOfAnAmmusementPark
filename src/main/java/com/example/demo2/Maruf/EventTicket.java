6bf300b59f665c3872cf894f936b91f3a9cc4100
Asadur Rahman Maruf <2210292@iub.edu.bd>
2025-04-14 12:36:05 +0600

    added model class by maruf

--- src/main/java/com/example/demo2/Maruf/EventTicket.java
+++ src/main/java/com/example/demo2/Maruf/EventTicket.java
@@ -1,4 +1,17 @@
 package com.example.demo2.Maruf;
 
 public class EventTicket {
+    private String eventTicketId;
+    private String eventName;
+    private String userId;
+    private float Price;
+    private String Status;
+    public EventTicket(String id,String name,String user,float p, String s){
+        eventTicketId = id;
+        eventName= name;
+        userId= user;
+        Price= p;
+        Status = s;
+    }
+
 }
