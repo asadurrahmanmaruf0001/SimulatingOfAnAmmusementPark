e125d83b7b2329069af36c848facb28581bb8fde
Asadur Rahman Maruf <2210292@iub.edu.bd>
2025-04-14 12:01:30 +0600

    added model class by maruf

--- src/main/java/com/example/demo2/Maruf/DemoController.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo2.Maruf;
-
-public class DemoController
-{
-    @javafx.fxml.FXML
-    public void initialize() {
-    }}
---
+++ src/main/java/com/example/demo2/Maruf/Ticket.java
@@ -0,0 +1,4 @@
+package com.example.demo2.Maruf;
+
+public class Ticket {
+}
