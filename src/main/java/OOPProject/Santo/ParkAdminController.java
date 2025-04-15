387f6b9b7042cd4f82b3085b470d6f56b5eed172
Santo-03 <shahrinrazzakshanto@gmail.com>
2025-04-15 11:16:37 +0600

    I am commiting Park Admin FXML

---
+++ src/main/java/OOPProject/Santo/ParkAdminController.java
@@ -0,0 +1,22 @@
+package OOPProject.Santo;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+
+public class ParkAdminController
+{
+    @javafx.fxml.FXML
+    private Button addRideButton;
+    @javafx.fxml.FXML
+    private ComboBox rideTypeCombo;
+    @javafx.fxml.FXML
+    private TextField rideIdField;
+    @javafx.fxml.FXML
+    private Button updateRideButton;
+    @javafx.fxml.FXML
+    private TextField rideNameField;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }}
