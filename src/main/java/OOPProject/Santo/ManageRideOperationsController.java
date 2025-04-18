e4c3a6433d5d747c96432a72b6da55fd006b2583
Santo-03 <shahrinrazzakshanto@gmail.com>
2025-04-18 16:57:34 +0600

    I am commiting ManageRideOperations.fxml

---
+++ src/main/java/OOPProject/Santo/ManageRideOperationsController.java
@@ -0,0 +1,53 @@
+package OOPProject.Santo;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+
+import javax.swing.table.TableColumn;
+
+public class ManageRideOperationsController
+{
+    @javafx.fxml.FXML
+    private TableColumn capacityColumn;
+    @javafx.fxml.FXML
+    private TableColumn rideNameColumn;
+    @javafx.fxml.FXML
+    private TableColumn rideTypeColumn;
+    @javafx.fxml.FXML
+    private Label ManageRideOperationsText;
+    @javafx.fxml.FXML
+    private TableView tableView;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void updateRideButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void RideTypeComboBox(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void RemoveRideButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void GenerateReportButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void AddRideButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void RideNAmeComboBox(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void CapacityComboBox(ActionEvent actionEvent) {
+    }
+}
